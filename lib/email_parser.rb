bf901ffacfc0ed933a0c10c1a86818ab1f1cb2e2
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-28 04:29:37 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -15,7 +15,7 @@ def initialize (emails)
   @@all << self
 end
 
-  def self.parse
+  def @parse
     email_list = emails.split(" , ")
     emails = email_list.collect do |email|
       @@all << emails
